2451d07a74d409dfcc1846c5bddada11e9e12f3c
cureseven <shiningcureseven@gmail.com>
2022-10-01 16:32:23 +0900

    select idにしてみた

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -1640,7 +1640,7 @@ func (h *Handler) updateDeck(c echo.Context) error {
 	}
 
 	// id再取得
-	query = "SELECT LAST_INSERT_ID()"
+	query = "SELECT id from user_decks ORDER BY id DESC LIMIT 1"
 	if err = tx.Get(newDeck.ID, query); err != nil {
 		return errorResponse(c, http.StatusInternalServerError, err)
 	}
