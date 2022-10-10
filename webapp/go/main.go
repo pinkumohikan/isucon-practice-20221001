c5829ea08d18d97a1246f709aaf0fa61b704f876
cureseven <shiningcureseven@gmail.com>
2022-10-10 08:31:51 +0000

    chore: stop log output

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -65,7 +65,7 @@ func main() {
 	time.Local = time.FixedZone("Local", 9*60*60)
 
 	e := echo.New()
-	e.Use(middleware.Logger())
+	//e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
